07abf25a6adb693dec2a32b15624ba21c9888316
VinchC <vinch.cm@gmail.com>
2024-10-21 16:09:02 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+};
